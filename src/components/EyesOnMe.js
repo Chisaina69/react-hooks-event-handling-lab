2bb4c23973c7d10be8cc15213318ddc48f4bb894
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-22 14:10:46 +0300

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    const hanleFocus = () => {
+        console.log('Good!');
+    }
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+    }
+    return(
+        <button onFocus={hanleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handlechange() {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handlechange}></input>
+            <button>Enter</button>
+        </div>
     )
 }
 
